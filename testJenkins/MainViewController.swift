5caa465f3712fc5cadbdd1b47e0b7dcfdd79754f
AHMED FRAAG <ahmedfraag255@gmail.com>
2019-10-25 17:00:48 +0200

    test push

--- testJenkins/MainViewController.swift
+++ testJenkins/MainViewController.swift
@@ -14,6 +14,7 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .red
+        print("test repo")
         
     }
     override func viewWillAppear(_ animated: Bool) {
